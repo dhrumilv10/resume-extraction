c0571ed196c5daeafd477fd8764870193176200c
Dhrumil Vadodaria <vadodariadhrumil@gmail.com>
2023-07-08 12:20:08 +0530

    create main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+from flask import Flask
+
+# app = Flask(__name__)
+
+
+class HelloWorld:
+    def __init__(self):
+        self.hello = "Hello World"
+
+    # @app.route("/")
+    def hello_world(self):
+        return self.hello
+
+
+if __name__ == "__main__":
+    # app.run(host="localhost", port=8000)
+    obj = HelloWorld()
+    print(obj.hello_world())
