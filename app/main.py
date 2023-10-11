af75bb936f7473222577b85756237922ef4e3f07
Dhrumil Vadodaria <vadodariadhrumil@gmail.com>
2023-10-11 12:13:08 +0530

    Flask added to code

--- app/main.py
+++ app/main.py
@@ -1,18 +1,11 @@
+import os
 from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("-------Starting app on PORT %d----------")
 
-class HelloWorld:
-    def __init__(self):
-        self.hello = "Hello World"
-
-    # @app.route("/")
-    def hello_world(self):
-        return self.hello
-
-
-if __name__ == "__main__":
-    # app.run(host="localhost", port=8000)
-    obj = HelloWorld()
-    print(obj.hello_world())
+    app.run(debug=True, port=port, host='0.0.0.0')
+    print("Hello World")
