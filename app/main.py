cd0a9e680a17551d7d7c19b547fee1ac7098beff
Dhrumil Vadodaria <vadodariadhrumil@gmail.com>
2023-10-11 12:18:15 +0530

    added utility folder

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# def
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    print("-------Starting app on PORT %d----------")
+    print("-------Starting app on PORT 5000----------")
 
     app.run(debug=True, port=port, host='0.0.0.0')
     print("Hello World")
